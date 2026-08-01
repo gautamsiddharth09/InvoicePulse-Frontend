01f2910c7818bcc6bf5557692f214db1a1ef3031
Gautam kumar <gautamsiddharth2013@gmail.com>
2026-08-01 15:38:19 +0530

    added date logic

--- src/utils/formDate.js
+++ src/utils/formDate.js
@@ -1,11 +1,27 @@
+// export const formatDate = (date) => {
+//   if (!date) return "";
+
+//   return new Date(date)
+//     .toLocaleDateString("en-GB", {
+//       day: "2-digit",
+//       month: "short",
+//       year: "numeric",
+//     })
+//     .replace(/ /g, "/");
+// };
+
+
 export const formatDate = (date) => {
   if (!date) return "";
 
-  return new Date(date)
-    .toLocaleDateString("en-GB", {
-      day: "2-digit",
-      month: "short",
-      year: "numeric",
-    })
-    .replace(/ /g, "/");
+  const d = new Date(date);
+
+  const day = String(d.getDate()).padStart(2, "0");
+  const month = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ][d.getMonth()];
+  const year = d.getFullYear();
+
+  return `${day}/${month}/${year}`;
 };
